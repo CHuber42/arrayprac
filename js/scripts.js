dd2cc1d7291c64f70c890d9a31ac119b30969fc7
Ethan Firpo <efirpo@gmail.com>
2020-03-30 10:15:23 -0700

    add initial javascript to assign survey results to array
    coauthored by Christopher Huber <chuber42.work@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
-array1 = ["Stark", "Banner", "Natasha", "Thor", "Cap"];
-array2 = ["Mom", "Michael", "Suzanne"];
+$(document).ready(function(){
+  $("form#favoritesForm").submit(function(event){
+    event.preventDefault();
+    
+    var favThings = [$("#things1").val(), $("#things2").val(), $("#things3").val(), $("#things4").val(), $("#things5").val()]
+    console.log(favThings);
+    var array2 = [];
+    array2.push(favThings[2]);
+    array2.push(favThings[0]); 
+    array2.push(favThings[1]);
+    console.log(array2);
+    array2.array.forEach(element => {
+      
+    });
+  });
+});
 
-array1.concat(array2);
-array1.push("Autumn", "Bagheera");
-
-array1.reverse();
-array1[2];
-
-for (i = 0; i < (array.length() - 1); i++){
-  if (array[i] === "Stark"){
-    console.log(array[i]);
-  }
-}
+var node = document.createElement("LI");                 // Create a <li> node
+var textnode = document.createTextNode("Water");         // Create a text node
+node.appendChild(textnode);                              // Append the text to <li>
+document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList"
