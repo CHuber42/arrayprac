819ba5f502de27f4be8daf807217390f710edef6
Ethan Firpo <efirpo@gmail.com>
2020-03-30 09:30:03 -0700

    add initial arrays for syntax practice
    coauthored by Christopher Huber <chuber42.work@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+array1 = ["Stark", "Banner", "Natasha", "Thor", "Cap"];
+array2 = ["Mom", "Michael", "Suzanne"];
+
+array1.concat(array2);
+array1.push("Autumn", "Bagheera");
+
+array1.reverse();
+array1[2];
+
+for (i = 0; i < (array.length() - 1); i++){
+  if (array[i] === "Stark"){
+    console.log(array[i]);
+  }
+}
